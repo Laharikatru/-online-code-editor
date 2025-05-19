a895535a81ab9ba744d5aab189fceb748db8fd19
Laharikatru <klaharinavya@gmail.com>
2025-05-19 16:24:21 +0530

    Update api.js

--- code-editor-frontendnew/src/api.js
+++ code-editor-frontendnew/src/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'https://your-backend-service.onrender.com'; // Replace with your actual backend URL
+const API_BASE_URL = 'https://online-code-editor-16.onrender.com'; // Replace with your actual backend URL
 
 export default API_BASE_URL;
